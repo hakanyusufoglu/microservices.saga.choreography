dbf2403cfe067e5575c050bd46ac161b6799b2ae
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-03-22 10:02:37 +0300

    Payment servisi için StockReservedEventConsumer consumerı oluşturuldu ve ödemenin başarılı ya da başarısız olması durumuna ait bir senaryo gerçekleştirildi Shared içerisinde PaymentCompletedEvent ve PaymentFailedEvent, eventleri gerçekleştirildi.

---
+++ Payment.Api/Consumers/StockReservedEventConsumer.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using Shared.Events;
+
+namespace Payment.Api.Consumers
+{
+    public class StockReservedEventConsumer(IPublishEndpoint publishEndpoint) : IConsumer<StockReservedEvent>
+    {
+        //Ödeme ile ilgili işlemler burada gerçekleştirilir. örneğin banka işlemleri vs yapılır ancak bu bir örnek sınıftır.
+        public async Task Consume(ConsumeContext<StockReservedEvent> context)
+        {
+            if (true)
+            {
+                //ödeme başarılı
+                PaymentCompletedEvent paymentCompletedEvent = new PaymentCompletedEvent
+                {
+                    OrderId = context.Message.OrderId
+                };
+                await publishEndpoint.Publish(paymentCompletedEvent);
+                await Console.Out.WriteLineAsync("Ödeme başarılı...");
+            }
+            else
+            {
+                //ödeme başarısız 
+                //Burada Cheropgraphy patterni kullanılır.
+                PaymentFailedEvent paymentFailedEvent = new()
+                {
+                    OrderId = context.Message.OrderId,
+                    Message = "Ödeme başarısız (ex. Yetersiz Bakiye)",
+                    OrderItemMessages = context.Message.OrderItems
+                };
+
+                await publishEndpoint.Publish(paymentFailedEvent);
+                await Console.Out.WriteLineAsync("Ödeme başarısız...");
+            }
+        }
+    }
+}
