2e1d7dd7b8519b24d062d2ad6195f263a90e4828
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-03-22 09:01:16 +0300

    Stock Api için mongodb entegrasyonu tamamlandı. Stock entity'si oluşturuldu.

---
+++ Stock.Api/Models/Stock.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Stock.Api.Models
+{
+    public class Stock
+    {
+        [BsonId]
+        [BsonGuidRepresentation(MongoDB.Bson.GuidRepresentation.CSharpLegacy)]
+        [BsonElement("Order=0")]
+        public Guid Id { get; set; }
+
+        [BsonGuidRepresentation(MongoDB.Bson.GuidRepresentation.CSharpLegacy)]
+        [BsonElement("Order=1")]
+        public Guid ProductId { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Int64)]
+        [BsonElement("Order=2")]
+        public int Count { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.DateTime)]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        [BsonElement("Order=2")]
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Stock.Api/Services/MongoDbService.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+
+namespace Stock.Api.Services
+{
+    public class MongoDbService
+    {
+        readonly IMongoDatabase _database;
+        public MongoDbService(IConfiguration configuration)
+        {
+            MongoClient client = new(configuration.GetConnectionString("MongoDb"));
+            _database=client.GetDatabase("StockDb");
+        }
+
+        public IMongoCollection<T> GetCollection<T>()=>_database.GetCollection<T>(typeof(T).Name.ToLowerInvariant());
+
+    }
+}
