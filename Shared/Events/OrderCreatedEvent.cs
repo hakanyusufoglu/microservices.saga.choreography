6e935dedafb219cf025b76920dfdccd565d5bf3f
Hakan Yusufoğlu <hyusufoglu@isbak.istanbul>
2024-03-22 08:26:14 +0300

    Stok servise order oluşturulduğuna dair olay publish etmek için OrderCreatedEvent ve OrderItemMessage sınıfları oluşturuldu

---
+++ Shared/Events/OrderCreatedEvent.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Messages;
+
+namespace Shared.Events
+{
+    //Eventler geçmişe aittir.
+    public class OrderCreatedEvent
+    {
+         //Buradaki veriler uyarıcağımız yani stock api'nin ihtiyayacı olacağı verileri eklemek mantıkılıdır.
+         public Guid OrderId { get; set; }
+        public Guid BuyerId { get; set; }
+        public decimal TotalPrice { get; set; }
+        public List<OrderItemMessage> OrderItems { get; set;}
+    }
+}
---
+++ Shared/Messages/OrderItemMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Messages
+{
+    //stock apinin ihtiyacı olacağı verileri barındıracak olan sınıf.
+    public class OrderItemMessage
+    {
+        public Guid ProductId { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+    }
+}
